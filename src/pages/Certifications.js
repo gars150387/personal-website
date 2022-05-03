c19d1973fed1d33e5aa11457e7fa5719c71d0639
Gustavo <gar.santeliz@gmail.com>
2022-05-02 23:16:49 -0400

    page to show data rendered

---
+++ src/pages/Certifications.js
@@ -0,0 +1,8 @@
+import React from "react";
+import {CertificationShow} from '../components/CertificationShow'
+
+export const Certifications =(props)=>{
+    return (
+        <CertificationShow key={props.id}/>
+    )
+}
