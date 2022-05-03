1b363d6b02835bd3420493ca8570fce963e277d9
Gustavo <gar.santeliz@gmail.com>
2022-05-02 23:10:45 -0400

    cleaning up data

---
+++ src/components/CertificationsData.js
@@ -0,0 +1,121 @@
+
+export const CertificationsData = [
+	{
+		id: "000",
+		title: "Full Stack Web Developer",
+		company: "Miami Dade College in partnered with 4Geeks Academy",
+		href: "https://www.mdc.edu/ce/technology/",
+		img: "FullStackWebDeveloperCertificationMiamiDadeCollege.jpg"
+	},
+	{
+		id: "001",
+		title: "Become a React Developer",
+		company: "LinkedIn Learning",
+		href: "https://www.linkedin.com/learning/paths/become-a-react-developer?u=107513458",
+		img: "BecomeAReactDeveloper.jpg",
+	},
+	{
+		id: "002",
+		title: "React: Testing and Debugging",
+		company: "LinkedIn Learning",
+		href:
+			"https://www.linkedin.com/learning/react-testing-and-debugging/testing-and-debugging-your-react-apps?autoAdvance=true&autoSkip=false&autoplay=true&contextUrn=urn%3Ali%3AlearningCollection%3A6820681459993583616&resume=true&u=107513458",
+		img: "ReactTestingAndDebugging.jpg",
+	},
+	{
+		id: "003",
+		title: "Firebase Essential Training",
+		company: "LinkedIn Learning",
+		href:
+			"https://www.linkedin.com/learning/firebase-essential-training/firebase-stop-reinventing-the-wheel?autoAdvance=true&autoSkip=false&autoplay=true&contextUrn=urn%3Ali%3AlearningCollection%3A6833404251495112704&resume=true&u=107513458",
+		img: "FirebaeEssentialTraining.jpg",
+	},
+	{
+		id: "004",
+		title: "Node.js Essential Training",
+		company: "LinkedIn Learning",
+		href:
+			"https://www.linkedin.com/learning/node-js-essential-training-web-servers-tests-and-deployment/go-beyond-the-basics-in-node?autoAdvance=true&autoSkip=false&autoplay=true&contextUrn=urn%3Ali%3AlearningCollection%3A6820681459993583616&resume=true&u=107513458",
+		img: "NodejsEssentialTraining.jpg",
+	},
+	{
+		id: "005",
+		title: "React Building an Interface",
+		company: "LinkedIn Learning",
+		href:
+			"https://www.linkedin.com/learning/react-js-building-an-interface-8551484/why-react-is-so-important?autoAdvance=true&autoSkip=false&autoplay=true&resume=true&u=107513458",
+		img: "ReactBuildingAnInterface.jpg",
+	},
+	{
+		id: "006",
+		title: "Developing a critical thinking mindset",
+		company: "LinkedIn Learning",
+		href:
+			"https://www.linkedin.com/learning/developing-a-critical-thinking-mindset/benefits-of-a-critical-thinking-mindset?autoAdvance=true&autoSkip=false&autoplay=true&resume=true&u=107513458",
+		img: "DevelopingACriticalThinkingMindset.jpg",
+	},
+	{
+		id: "007",
+		title: "React Design Patterns",
+		company: "LinkedIn Learning",
+		href:
+			"https://www.linkedin.com/learning/react-design-patterns/take-your-react-skills-to-the-next-level?autoAdvance=true&autoSkip=false&autoplay=true&resume=true&u=107513458",
+		img: "ReactDesignPatterns.png",
+	},
+	{
+		id: "008",
+		title: "React Creating and Hosting a Full-Stack site",
+		company: "LinkedIn Learning",
+		href:
+			"https://www.linkedin.com/learning/react-creating-and-hosting-a-full-stack-site/react-for-full-stack-solutions?autoAdvance=true&autoSkip=false&autoplay=true&resume=true&u=107513458",
+		img: "ReactCreatingAndHostingAFullStackSite.jpg",
+	},
+	{
+		id: "009",
+		title: "React Hooks",
+		company: "LinkedIn Learning",
+		href:
+			"https://www.linkedin.com/learning/react-hooks/understanding-modern-react?autoAdvance=true&autoSkip=false&autoplay=true&resume=true&u=107513458",
+		img: "ReactHooks.jpg",
+	},
+	{
+		id: "010",
+		title: "React Ecosystems",
+		company: "LinkedIn Learning",
+		href:
+			"https://www.linkedin.com/learning/react-ecosystems/welcome?autoAdvance=true&autoSkip=false&autoplay=true&resume=true&u=107513458",
+		img: "ReactEcosystems.jpg",
+	},
+	{
+		id: "011",
+		title: "React Components Context and Accessibility",
+		company: "LinkedIn Learning",
+		href:
+			"https://www.linkedin.com/learning/react-components-context-and-accessibility/welcome?autoAdvance=true&autoSkip=false&autoplay=true&resume=true&u=107513458",
+		img: "ReactComponentsContextAndAccessibility.jpg",
+	},
+	{
+		id: "012",
+		title: "JavaScript Code Challenges",
+		company: "LinkedIn Learning",
+		href:
+			"https://www.linkedin.com/learning/javascript-code-challenges/put-your-javascript-skills-to-the-test?autoAdvance=true&autoSkip=false&autoplay=true&resume=true&u=107513458",
+		img: "JavascriptCodeChallenges.jpg",
+	},
+	{
+		id: "013",
+		title: "React Essential Training",
+		company: "LinkedIn Learning",
+		href:
+			"https://www.linkedin.com/learning/react-js-essential-training/building-modern-user-interfaces-with-react?autoAdvance=true&autoSkip=false&autoplay=true&resume=true&u=107513458",
+		img: "ReactjsEssentialTraining.jpg",
+	},
+	// {
+	// 	id: "014",
+	// 	title: "React Essential Training",
+	// 	company: "LinkedIn Learning",
+	// 	href:
+	// 		"https://www.linkedin.com/learning/react-js-essential-training/building-modern-user-interfaces-with-react?autoAdvance=true&autoSkip=false&autoplay=true&resume=true&u=107513458",
+	// 	img: "ReactEssentialTraining"
+	// }
+];
