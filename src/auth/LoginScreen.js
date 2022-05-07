31b0ebad8944a499abface590f3ca88c556ad1fe
Gustavo <gar.santeliz@gmail.com>
2022-05-07 12:44:07 -0400

    internal components set to process with login

--- src/auth/LoginScreen.js
+++ src/auth/LoginScreen.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Container, Row, Stack, Col, Button, ButtonGroup, Card } from 'react-bootstrap'
+import { Container, Row, Stack, Col, Button, ButtonGroup, Card, Form, Nav } from 'react-bootstrap'
 import { FaFacebook, FaGithub, FaTwitter } from 'react-icons/fa'
 import { SiGmail } from 'react-icons/si'
 
@@ -11,14 +11,18 @@ export const LoginScreen = () => {
       <Row>
         <Stack>
           <ButtonGroup className='mt-2'>
-            <Button variant='light' className='m-4'>Register</Button>
-            <Button variant="primary" className='m-4'>Login</Button>
+          <Nav.Link href='/auth/register'>
+                <Button variant='light' className='m-4'>Register</Button>
+              </Nav.Link>
+              <Nav.Link href='/auth/login'>
+                <Button variant='light' className='m-4'>Login</Button>
+              </Nav.Link>
           </ButtonGroup>
         </Stack>
       </Row>
       <Row>
         <Col>
-          <Card.Title>You can sign in with:</Card.Title>
+          <Card.Title>Sign in with:</Card.Title>
           <br />
           <FaFacebook className='icon' />
           <SiGmail className='icon' />
@@ -29,18 +33,35 @@ export const LoginScreen = () => {
         </Col>
         <Row>
           <Col>
-          <Card.Title>Email Address</Card.Title>
-            <input placeholder='email address'/>
+            <Card.Title>Email Address</Card.Title>
+            <input placeholder='email address' />
 
             <Card.Title>Password:</Card.Title>
-            <input placeholder='password'/>
+            <input placeholder='password' />
           </Col>
           <Row>
             <Col>
-              
+              <Form>
+                  <div className="mb-3">
+                    <Form.Check
+                      type='checkbox'
+                      id='checkbox'
+                      label='Remember me'
+                    />
+                  </div>
+              </Form>
+
+              <Nav.Link href="">Forget Password?</Nav.Link>
+
             </Col>
           </Row>
         </Row>
+        <Button>SIGN IN</Button>
+      </Row>
+      <Row>
+      <Card.Title>Not a member? 
+      </Card.Title>
+      <Nav.Link href="">Register</Nav.Link>
       </Row>
     </Container>
   )
