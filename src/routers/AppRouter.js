654fef0c0d764d07ec29d98e28a77652592f9ae6
Gustavo <gar.santeliz@gmail.com>
2022-05-07 14:46:29 -0400

    public routes added

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -9,12 +9,19 @@ import { Home } from '../pages/Home'
 import { AuthRouter } from './AuthRouter';
 import { RegisterScreen } from '../auth/RegisterScreen';
 import { LoginScreen } from '../auth/LoginScreen';
+import { WhoAmI } from '../pages/WhoAmI';
+import { MyBackground } from '../pages/MyBackground';
+import { Contact } from '../pages/Contact';
+
 
 export const AppRouter = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route exact path="/" element={<Home />} />
+                <Route exact path="/whoami" element={<WhoAmI />} />
+                <Route exact path="/mybackground" element={ <MyBackground />} />
+                <Route exact path="/contact" element={<Contact />} />
                 <Route exact path="/auth" element={<AuthRouter />}>
                     <Route
                         exact
