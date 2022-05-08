3b1c9aa35a125e8de19683975f6c21f50b6e194a
Gustavo <gar.santeliz@gmail.com>
2022-05-07 23:44:22 -0400

    provider and store added to pass the data from the highest level

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import { Provider } from "react-redux";
+
 import { AppRouter } from './routers/AppRouter'
+import { store } from './store/store';
+
 
 export const App = () => {
   return (
-    <div>
+    <Provider store={ store }>
         <AppRouter />
-    </div>
+    </Provider>
   )
 }
