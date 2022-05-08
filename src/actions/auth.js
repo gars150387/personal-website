0c568710d7fa4a153c795d72dbc593888b4dda6e
Gustavo <gar.santeliz@gmail.com>
2022-05-08 18:57:00 -0400

    set google sign in

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,14 +1,23 @@
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { type } from "../types/type"
+import { googleAuthProvider } from '../firebase/firebase-config'
 
-
-export const startLoginEmailPassword = (email, password) => {
-    return async (dispatch) => {
+export const startLoginEmailPassword = (email, password) =>{
+    return (dispatch) =>{
         setTimeout(() => {
-            dispatch(123456, 'Pedro')
-        }, 1500)
+            dispatch(login(123, 'Pedro'))
+        }, 3500);
     }
 }
-
+export const startGoogleLogin = () =>{
+    return (dispatch) =>{
+        const auth = getAuth();
+        signInWithPopup(auth, googleAuthProvider)
+            .then(({user}) =>{
+                dispatch(login(user.uid, user.displayName))
+            });
+    }
+}         
 
 export const login = (uid, displayName) => {
     return {
