de8901efc13a9351ba7f4c83d477be07953b2c7b
Gustavo <gar.santeliz@gmail.com>
2022-05-15 16:36:37 -0400

    navbar created to search thru data

---
+++ src/components/NavbarInner.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import { CertificationsData } from './CertificationsData'
+
+
+export const NavbarInner = () => {
+
+    const [value, setvalue] = useState('')
+    
+    const result = CertificationsData.find(({ title }) => title === value)
+    console.log( result )
+
+    const handleSearch = (e) =>{
+        setvalue(e.target.value)
+    }
+
+    const handleSubmit = () =>{
+        console.log(value)
+    }
+    
+    return (
+        <>
+           <input
+           name="value"
+           onChange={handleSearch}
+           value={ value }
+           placeholder="Search here"
+            />
+
+            <button onClick={handleSubmit}>Search</button>
+        </>
+    )
+}
