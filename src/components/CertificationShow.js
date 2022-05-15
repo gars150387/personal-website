9afd867315917283741814700465d1ee208c7dca
Gustavo <gar.santeliz@gmail.com>
2022-05-15 16:36:07 -0400

    working on filter data rendered

--- src/components/CertificationShow.js
+++ src/components/CertificationShow.js
@@ -1,91 +1,39 @@
 import React from 'react';
-import { Button, Card, Stack } from 'react-bootstrap';
+import { Stack } from 'react-bootstrap';
 import { CertificationsData } from './CertificationsData';
+import { CertificateFormat } from './CertificateFormat';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
 
 
 
 export const CertificationShow = () => {
 
-   
-    let OneRow = [];
-
-    for (let i = 0; i < CertificationsData.length; i += 3) {
-        OneRow.push(CertificationsData.slice(i, i + 3).map(certificate => {
-            return certificate
-        }))
-    }
-
+    const [serachTerm, setserachTerm] = useState('')
 
     return (
         <>
-            <Card.Title>Certifications</Card.Title>
             <Stack gap={3}>
                 {
-                    OneRow.map(row => {
-                        console.log('one row', row )
+                    Object.values(CertificationsData).filter((values)=>{
+                        if( serachTerm === ''){
+                            return values
+                        } else if (values.title.toLowerCase().includes(serachTerm.toLocaleLowerCase())){
+                            return values
+                        }
+                    }).map(({ id, title, company, href, img }) => {
                         return (
-
-                            Object.values(row.map(key => {
-                                console.log( 'key', key)
-                                return (
-                                    <div>
-                                        <Card sm key={key.id} style={{
-                                            width: '35vw',
-                                            margin: 'auto',
-                                            marginTop: '2%',
-                                            borderColor: '#212529',
-                                            borderTopColor: 'transparent',
-                                            borderLeftColor: 'transparent',
-                                            borderRightColor: 'transparent',
-                                            borderRadius: '5px'
-                                        }}>
-                                            <Card.Title className='text-center' style={{
-                                                width: '35vw',
-                                                marginTop: '2%',
-                                                fontSize: '25px',
-
-                                            }}>
-                                                {key.title}
-                                            </Card.Title>
-                                            <Card.Img id="certificationsImage" src={require(`../images/${key.img}`)} alt={`${key.img}`} style={{
-                                                width: '35vw',
-                                                borderColor: '#212529',
-                                                borderRadius: '5px',
-                                                backgroundColor: '#212529'
-                                            }} />
-                                            <Card.Body>
-                                                <Card.Text className='text-center' style={{
-                                                    width: '32vw',
-                                                    marginTop: '2%',
-                                                    fontSize: '25px',
-                                                    textDecoration: 'underline',
-                                                    boxShadow: '-2px 3px 5px #212529'
-                                                }}>
-                                                    {key.company}
-                                                </Card.Text>
-                                            </Card.Body>
-                                            <Button variant="success" href={key.href} style={{
-                                                borderColor: '#212529',
-                                                border: 'solid 1px',
-                                                boxShadow: '-2px 3px 5px #212529'
-                                            }}>Check out</Button>
-                                        </Card>
-                                    </div>
-                                )
-
-                            })
-                            )
+                            <Stack direction='horizontal' xs lg={2}>
+                                <CertificateFormat key={id} title={title} company={company} href={href} img={img} />
+                            </Stack>
                         )
                     })
                 }
-
             </Stack>
+
+
 
         </>
     );
 }
-
-
-
