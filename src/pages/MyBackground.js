81a522dafa60f5f7100acf3d3597b7485d488052
Gustavo <gar.santeliz@gmail.com>
2022-05-17 19:28:45 -0400

    final components imported to render sorted data

--- src/pages/MyBackground.js
+++ src/pages/MyBackground.js
@@ -1,16 +1,22 @@
 import React from "react";
-
-import { NavbarHeader } from "../components/Navbar";
 import { CertificationShow } from '../components/CertificationShow'
+import { Stack, Col, Card } from "react-bootstrap";
 
 import '../styles/mybackground.css'
+import { ScrollUp } from "../hooks/useScrollUp";
+
 
 export const MyBackground = (props) => {
     return (
 
         <div className="myBackground-body">
-            <NavbarHeader />
-            <CertificationShow key={props.id} />
+            <Stack>
+                <Col>
+                    <Card.Title className="text-center"><h1><span>My Certificaions</span></h1></Card.Title>
+                </Col>
+                <ScrollUp />
+                <CertificationShow key={props.id} />
+            </Stack>
         </div>
     )
 }
