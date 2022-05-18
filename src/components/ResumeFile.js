04ad15607447342320e3d4b86b53a0100f371fbc
Gustavo <gar.santeliz@gmail.com>
2022-05-18 19:38:29 -0400

    viewer set to render pdf file

--- src/components/ResumeFile.js
+++ src/components/ResumeFile.js
@@ -1,27 +1,46 @@
 import React, { useState } from 'react'
 import { Viewer } from '@react-pdf-viewer/core'
-// import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout'
+import { Worker } from '@react-pdf-viewer/core'
+import { Col, Button } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+
 import "@react-pdf-viewer/core/lib"
 import '@react-pdf-viewer/default-layout'
 
-import { Worker } from '@react-pdf-viewer/core'
+import '@react-pdf-viewer/core/lib/styles/index.css';
+import '@react-pdf-viewer/default-layout/lib/styles/index.css';
 
+import '../styles/resume.css'
+
+import { ScrollUp } from '../hooks/useScrollUp'
 import PdfFile from '../pdf/GustavoRodriguezSanteliz_SoftwareDeveloper.PDF'
 
-export const ResumeFile = () =>{
+export const ResumeFile = () => {
 
     const [defaultPdfFile] = useState(PdfFile);
 
-    // const defaultLayoutPluginInstance = defaultLayoutPlugin()
 
     return (
         <div className="pdf-container">
-            {defaultPdfFile && <> <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.6.347/build/pdf.worker.min.js">
-            <Viewer className="viewer" fileUrl={defaultPdfFile}
-        // plugins={[defaultLayoutPluginInstance]}
-        
-        />
-        </Worker></>}
+            <Col md={{ span: 8, offset: 2 }} className='text-center'>
+                <Link to="/mybackground/certificates">
+                    <Button variant='secondary' style={{
+                        width: '50vw',
+                    }}>Get me out!</Button>
+                </Link>
+            </Col>
+            <ScrollUp />
+            {defaultPdfFile &&
+                <>
+                    <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.6.347/build/pdf.worker.min.js">
+                        <Viewer className="viewer" fileUrl={defaultPdfFile} />
+                    </Worker>
+                </>}
         </div>
     )
 }
+
+
+//https://unpkg.com/pdfjs-dist@2.6.347/build/pdf.worker.min.js video mp4 link
+
+//import PdfFile from '../pdf/GustavoRodriguezSanteliz_SoftwareDeveloper.PDF'
