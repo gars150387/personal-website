db87902fa184c96ce3bb5f6d6d4385ea227368e4
Gustavo <gar.santeliz@gmail.com>
2022-05-19 18:53:35 -0400

    path redisigned

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -13,7 +13,6 @@ import { WhoAmI } from '../pages/WhoAmI';
 import { MyBackground } from '../pages/MyBackground';
 import { Contact } from '../pages/Contact';
 import { Resume } from '../pages/Resume';
-import { InnerRoute } from './InnerRoute';
 
 
 export const AppRouter = () => {
@@ -22,11 +21,8 @@ export const AppRouter = () => {
             <Routes>
                 <Route exact path="/" element={<Home />} />
                 <Route exact path="/whoami" element={<WhoAmI />} />
-                {/* <Route exact path="/mybackground" element={ <MyBackground />} /> */}
-                <Route exact path='/mybackground' element={<InnerRoute />} >
-                    <Route exact path='resume' element={<Resume />} />
-                    <Route exact path='certificates' element={<MyBackground />}/>
-                </Route>
+                <Route exact path="/mybackground" element={<MyBackground />} />
+                <Route exact path='/resume' element={<Resume />} />
                 <Route exact path="/contact" element={<Contact />} />
                 <Route exact path="/auth" element={<AuthRouter />}>
                     <Route
