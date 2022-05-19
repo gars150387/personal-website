ed539ba3fa1bed1f4bf2e1c46a2fcf8d63bade0e
Gustavo <gar.santeliz@gmail.com>
2022-05-18 20:05:43 -0400

    more contacts added

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { ContactMe } from "../components/ContactForm";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Card, Button} from "react-bootstrap";
 import { NavbarHeader } from "../components/Navbar";
 
 
@@ -9,11 +9,10 @@ import '../styles/contact.css'
 
 export const Contact = () => {
     return (
-        <Container>
-                        <NavbarHeader />
-            <br />
-
-            <Row>
+        <>
+                <Container className="contact-container" fluid>
+            <NavbarHeader />
+            <Row className="mt-5">
                 <Col md={{ span: 6, offset: 3 }}>
                     <Row>
                         <Col className="text-center">
@@ -21,13 +20,45 @@ export const Contact = () => {
                         </Col>
                     </Row>
                     <Row>
-                        <Col className="box-message">
-                            <ContactMe/>
+                        <Col style={{
+                            width: '150%',
+                            height: '150%'
+                        }} className="box-message">
+                            <ContactMe />
                         </Col>
                     </Row>
                 </Col>
             </Row>
         </Container >
+        <Container>
+        <Row>
+            <Col md={{ span: 6, offset: 3 }}>
+                
+                <Card.Header><h1>Others link to contact me</h1></Card.Header>
+                <Card style={{
+                    width: '32vw',
+                    position: 'absolute',
+                    left: '34%',
+                    color: 'white',
+                }}>
+                    <Card.Body style={{
+                        width: '10vw'
+                    }}>
+                        <Card.Title>LinkedIn</Card.Title>
+                        <Button target="_blank" rel="noopener noreferrer"  href='https://www.linkedin.com/in/garsanteliz' variant="primary">CLick here</Button>
+                    </Card.Body>
+                    <hr />
+                    <Card.Body>
+                        <Card.Title>GitHub</Card.Title>
+                        <Button target="_blank" rel="noopener noreferrer"  href='https://www.github.com/gars150387' variant="primary">CLick here</Button>
+                    </Card.Body>
+                </Card>
+            </Col>
+            </Row>
+
+        </Container>
+
+        </>
     )
 }
 
