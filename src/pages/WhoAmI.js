6a2ac75ae675d877d27683e8e0dc2d00ac1074df
Gustavo <gar.santeliz@gmail.com>
2022-05-19 23:08:56 -0400

    last commit

--- src/pages/WhoAmI.js
+++ src/pages/WhoAmI.js
@@ -4,7 +4,6 @@ import { NavbarHeader } from '../components/Navbar'
 
 import '../images/sanFranciscoTrip.jpg'
 import '../images/carrying-a-skateboard.jpeg'
-
 import '../styles/whoami.css'
 
 export const WhoAmI = () => {
@@ -15,11 +14,11 @@ export const WhoAmI = () => {
                         <div className="col-md-8">
                             <div className="card-body">
                                 <h5 className="card-title"><h1>Here who I am!</h1></h5>
-                                <p className="card-text">Hi everyone, my name is Gustavo and I am a brand new Software Developer, all my background is based on Marketing and Management but I started to move my career toward to my new goal as professional.</p>
-                                <p className="card-text">I started building my skillset as software developer deciding to attempt a coding bootcamp to get my basis foundamentals.</p>
-                                <p className="card-text">I always look for new challenge to help me to growth as a personas as professional</p>
+                                <p className="card-text">Hi everyone, my name is Gustavo and I am a brand new Software Developer, all my background is based in Marketing and Management but I started to move my career toward my new goal as a professional.</p>
+                                <p className="card-text">I started building my skillset as a software developer deciding to attempt a coding Bootcamp to get my basic fundamentals.</p>
+                                <p className="card-text">I always look for new challenges to help me to grow as a person as professional</p>
                                 <h5 className="card-title">This is how I enjoy my time</h5>
-                                <p className="card-text">I consider myself as an outdoor person, I like to go out and enjoy of the nature around me. I like to go out and spend time doing skateboarding on the street or at the pump track.</p>
+                                <p className="card-text">I consider myself an outdoor person, I like to go out and enjoy the nature around me. I like to go out and spend time skateboarding on the street or at the pump track.</p>
                                 <p className="card-text">Also, I like to do skimboarding and enjoy the time on the beach and try to keep up a balanced way of life.</p>
                             </div>
                         </div>
