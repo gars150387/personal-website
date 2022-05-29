fca3ec0234adf7000d6a2814fec1820a4ff9b45d
Gustavo <gar.santeliz@gmail.com>
2022-05-29 16:00:18 -0400

    cleaning up unused files

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,9 +1,5 @@
 import React from "react";
 import { ResumeFile } from "../components/ResumeFile";
-// import SinglePagePDFViewer from "./components/pdf/single-page";
-
-/* This is required only if the project file is located 
-inside the app. Otherwise you can use the external link of the pdf file*/
 
 export const Resume = ()=> {
   return (
