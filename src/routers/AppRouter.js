a7fa98a60de1db6ef58d5cf7f6522496a0738d25
Gustavo <gar.santeliz@gmail.com>
2022-05-29 23:28:56 -0400

    clean up variables declared but never used

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -5,10 +5,6 @@ import {
     Route,
 } from "react-router-dom"
 
-import { Home } from '../pages/Home'
-// import { AuthRouter } from './AuthRouter';
-// import { RegisterScreen } from '../auth/RegisterScreen';
-// import { LoginScreen } from '../auth/LoginScreen';
 import { WhoAmI } from '../pages/WhoAmI';
 import { MyBackground } from '../pages/MyBackground';
 import { Contact } from '../pages/Contact';
@@ -19,23 +15,10 @@ export const AppRouter = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route exact path="/" element={<Home />} />
                 <Route exact path="/whoami" element={<WhoAmI />} />
                 <Route exact path="/mybackground" element={<MyBackground />} />
                 <Route exact path='/resume' element={<Resume />} />
                 <Route exact path="/contact" element={<Contact />} />
-                {/* <Route exact path="/auth" element={<AuthRouter />}>
-                    <Route
-                        exact
-                        path='register'
-                        element={<RegisterScreen />}
-                    />
-                    <Route
-                        exact
-                        path='login'
-                        element={<LoginScreen />}
-                    />
-                </Route> */}
             </Routes>
         </BrowserRouter>
     )
