50a94bdeac4dab7697edd61c62c73a98e5755bea
Gustavo <gar.santeliz@gmail.com>
2022-05-30 14:14:57 -0400

    add link to navigate thru components

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,14 +12,14 @@ export const NavbarHeader = () => {
             <Nav className='vertical-nav' expand="lg" bg="dark">
                 <Container className='navbar-container'>
                     <Link to='/whoami'>
-                        <Nav.Link id='navbarMain-option'><p>Who Am I?</p></Nav.Link>
+                        <Nav.Link id='navbarMain-option' href='/whoami'><p>Who Am I?</p></Nav.Link>
                     </Link>
                     <Link to="/mybackground">
-                        <Nav.Link id='navbarMain-option'><p>My Background</p></Nav.Link>
+                        <Nav.Link id='navbarMain-option' href='/mybackground'><p>My Background</p></Nav.Link>
                     </Link>
                     <Nav.Link id='navbarMain-option' target="_blank" rel="noopener noreferrer" href='https://www.github.com/gars150387'><p>My GitHub</p></Nav.Link>
                     <Link to="/contact">
-                        <Nav.Link id='navbarMain-option' ><p>Contact</p></Nav.Link>
+                        <Nav.Link id='navbarMain-option' href='/contact' ><p>Contact</p></Nav.Link>
                     </Link>
                 </Container>
             </Nav>
