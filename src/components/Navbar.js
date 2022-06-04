bb3e7b7b56c9c074e8183b26d1272fc9b3880808
Gustavo <gar.santeliz@gmail.com>
2022-06-03 21:13:29 -0400

    setting up app to be responsaive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Container, Nav } from 'react-bootstrap'
+import { FaQuestionCircle, FaGithub } from 'react-icons/fa'
+import {GrCertificate, GrContact} from 'react-icons/gr'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -12,14 +14,14 @@ export const NavbarHeader = () => {
             <Nav className='vertical-nav' expand="lg" bg="dark">
                 <Container className='navbar-container'>
                     <Link to='/whoami'>
-                        <Nav.Link id='navbarMain-option' href='/whoami'><p>Who Am I?</p></Nav.Link>
+                        <Nav.Link id='navbarMain-option' href='/whoami'><p>Who Am I <FaQuestionCircle className='icons' /></p></Nav.Link>
                     </Link>
                     <Link to="/mybackground">
-                        <Nav.Link id='navbarMain-option' href='/mybackground'><p>My Background</p></Nav.Link>
+                        <Nav.Link id='navbarMain-option' href='/mybackground'><p>My Background <GrCertificate className='icons'  /></p></Nav.Link>
                     </Link>
-                    <Nav.Link id='navbarMain-option' target="_blank" rel="noopener noreferrer" href='https://www.github.com/gars150387'><p>My GitHub</p></Nav.Link>
+                    <Nav.Link id='navbarMain-option' target="_blank" rel="noopener noreferrer" href='https://www.github.com/gars150387'><p>My GitHub <FaGithub className='icons'  /></p></Nav.Link>
                     <Link to="/contact">
-                        <Nav.Link id='navbarMain-option' href='/contact' ><p>Contact</p></Nav.Link>
+                        <Nav.Link id='navbarMain-option' href='/contact' ><p>Contact <GrContact className='icons'  /></p></Nav.Link>
                     </Link>
                 </Container>
             </Nav>
