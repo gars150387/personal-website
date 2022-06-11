f9d72a7f72757f8305897aa1c0cd8d5a374600dc
Gustavo <gar.santeliz@gmail.com>
2022-06-11 12:20:22 -0400

    adding toLowerCase() feature to search input

--- src/components/CertificationShow.js
+++ src/components/CertificationShow.js
@@ -32,7 +32,7 @@ export const CertificationShow = () => {
             <div className='certificate-container'>
                 {
                     CertificationsData.filter(item =>
-                        item.title.toLowerCase().includes(searchTerm))
+                        item.title.toLowerCase().includes(searchTerm.toLowerCase()))
                         .map(item => {
                             return (
                                 <CertificateFormat key={item.id} title={item.title} company={item.company} href={item.href} img={item.img} />
