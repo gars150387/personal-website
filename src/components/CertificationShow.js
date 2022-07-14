bd5f8d19ba3eaab4dcc9b8333f4e1abaf142c015
Gustavo <gar.santeliz@gmail.com>
2022-07-14 19:18:44 -0400

    styling certificate container

--- src/components/CertificationShow.js
+++ src/components/CertificationShow.js
@@ -4,26 +4,24 @@ import { CertificateFormat } from "./CertificateFormat";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export const CertificationShow = ({ searchTerm }) => {
-  
-
   return (
     <>
       <div className="justify-content-center">
-      </div>
-      <div className="certificate-container">
-        {CertificationsData.filter((item) =>
-          item.title.toLowerCase().includes(searchTerm.toLowerCase())
-        ).map((item) => {
-          return (
-            <CertificateFormat
-              key={item.id}
-              title={item.title}
-              company={item.company}
-              href={item.href}
-              img={item.img}
-            />
-          );
-        })}
+        <div className="certificate-container">
+          {CertificationsData.filter((item) =>
+            item.title.toLowerCase().includes(searchTerm.toLowerCase())
+          ).map((item) => {
+            return (
+              <CertificateFormat
+                key={item.id}
+                title={item.title}
+                company={item.company}
+                href={item.href}
+                img={item.img}
+              />
+            );
+          })}
+        </div>
       </div>
     </>
   );
