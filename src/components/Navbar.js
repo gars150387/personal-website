984d32931010462837cd25e45847bb90d6a369ea
gars150387 <gar.santeliz@gmail.com>
2023-01-07 17:34:54 -0500

    scroll  function removed and new structure added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,111 +1,97 @@
 import React, { useState } from "react";
-import { Button } from "react-bootstrap";
-import { FaQuestionCircle, FaGithub, FaNetworkWired } from "react-icons/fa";
-import { GrCertificate, GrContact } from "react-icons/gr";
-import { Link } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
+import { SocialIcons } from "./SocialIcons"
 import "../styles/Sass/navbar.scss";
+import { Link } from "react-router-dom";
 
 export const NavbarHeader = () => {
-  const [display, setDisplay] = useState(true);
-  window.onscroll = function () {
-    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    console.log("Scroll position: " + scrollTop + "px");
-    if (scrollTop > 0) {
-      return setDisplay(false);
-    } else {
-      return setDisplay(true);
-    }
-  };
+  const [display, setDisplay] = useState(false);
+  const activeTab = window.location.hash;
 
   return (
-    <>
-      <nav
-        className={`navbar bg-body-none fixed-top d-${
-          display === false ? "none" : "visible"
-        }`}
-      >
-        <div className="container-fluid">
-          <div className="navbar-brand"></div>
+    <div className="container">
+      <nav className="navbar navbar-expand-lg">
+        <div className="container">
           <button
+            onClick={() => setDisplay(!display)}
             className="navbar-toggler"
             type="button"
-            data-bs-toggle="offcanvas"
-            data-bs-target="#offcanvasNavbar"
-            aria-controls="offcanvasNavbar"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarTogglerDemo02"
+            aria-controls="navbarTogglerDemo02"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
           >
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div
-            className="offcanvas offcanvas-end"
-            tabIndex="-1"
-            id="offcanvasNavbar"
-            aria-labelledby="offcanvasNavbarLabel"
-          >
-            <div className="offcanvas-header">
-              <h5 className="offcanvas-title" id="offcanvasNavbarLabel">
-                G.A.R.S
-              </h5>
-              <button
-                type="button"
-                className="btn-close"
-                data-bs-dismiss="offcanvas"
-                aria-label="Close"
-              ></button>
-            </div>
-            <div className="offcanvas-body">
-              <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
-                <Link to="/whoami">
-                  <div id="navbarMain-option" href="/whoami">
-                    <p>
-                      Who Am I <FaQuestionCircle className="icons" />
-                    </p>
-                  </div>
-                </Link>
-                <Link to="/mybackground">
-                  <div id="navbarMain-option" href="/mybackground">
-                    <p>
-                      My Background <GrCertificate className="icons" />
-                    </p>
-                  </div>
-                </Link>
-                <div
-                  id="navbarMain-option"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  href="https://www.github.com/gars150387"
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a
+                  className={`nav-link ${
+                    activeTab === "#who" ? "active" : null
+                  } `}
+                  href="#who"
                 >
-                  <p>
-                    My GitHub <FaGithub className="icons" />
-                  </p>
-                </div>
-                <Link to="/contact">
-                  <div id="navbarMain-option" href="/contact">
-                    <p>
-                      Contact <GrContact className="icons" />
-                    </p>
-                  </div>
-                </Link>
-                <Link to="/project">
-                  <div id="navbarMain-option" href="/project">
-                    <p>
-                      Projects <FaNetworkWired className="icons" />
-                    </p>
-                  </div>
-                </Link>
-                <Link to="/resume">
-                  <div className="button-container-resume">
-                    <Button className="button-certificate-link">
-                      <p>Resume </p>
-                      <i className="bi bi-file-person"></i>
-                    </Button>
-                  </div>
-                </Link>
-              </ul>
+                  Who I am?
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className={`nav-link ${
+                    activeTab === "#projects" ? "active" : null
+                  } `}
+                  href="#projects"
+                >
+                  Projects
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className={`nav-link ${
+                    activeTab === "#experience" ? "active" : null
+                  } `}
+                  href="#experience"
+                >
+                  Background
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className={`nav-link ${
+                    activeTab === "#contact" ? "active" : null
+                  } `}
+                  href="#contact"
+                >
+                  Contact
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className={`nav-link ${
+                    activeTab === "#background" ? "active" : null
+                  } `}
+                  href="#background"
+                >
+                  Certifications
+                </a>
+              </li>
+              <Link to="/resume">
+              <li className="nav-item">
+                <a
+                  className="nav-link"
+                  href="#background"
+                >
+                  Resume
+                </a>
+              </li>{" "}
+              </Link>
+            </ul>
+            <div className="d-flex" role="search">
+             <SocialIcons />
             </div>
           </div>
         </div>
       </nav>
-    </>
+    </div>
   );
 };
