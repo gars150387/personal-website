f989837c56ca733080e31311e9f6d231d041182f
gars150387 <gar.santeliz@gmail.com>
2023-01-07 17:38:11 -0500

    set up 3 routes only

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,29 +1,18 @@
-import React from 'react';
-import {
-    BrowserRouter,
-    Routes,
-    Route,
-} from "react-router-dom"
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import { WhoAmI } from '../pages/WhoAmI';
-import { MyBackground } from '../pages/MyBackground';
-import { Contact } from '../pages/Contact';
-import { Resume } from '../pages/Resume';
-import { Home } from '../pages/Home';
-import { Projects } from '../pages/Projects';
-
+import { Home } from "../pages/Home";
+import { MainPage } from "../pages/main-page";
+import { Resume } from "../pages/Resume";
 
 export const AppRouter = () => {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route exact path="/" element={<Home />} />
-                <Route exact path="/whoami" element={<WhoAmI />} />
-                <Route exact path="/mybackground" element={<MyBackground />} />
-                <Route exact path='/resume' element={<Resume />} />
-                <Route exact path="/contact" element={<Contact />} />
-                <Route exact path="/project" element={<Projects />} />
-            </Routes>
-        </BrowserRouter>
-    )
-}
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/main" element={<MainPage />} />
+        <Route exact path="/resume" element={<Resume />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
