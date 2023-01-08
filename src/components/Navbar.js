2ab75398dc63a4d688ebcdfa75be67e7e9c2c3f1
gars150387 <gar.santeliz@gmail.com>
2023-01-07 21:13:30 -0500

    navbar structure upated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,54 +1,30 @@
 import React, { useState } from "react";
-import { SocialIcons } from "./SocialIcons"
+import { SocialIcons } from "./SocialIcons";
 import "../styles/Sass/navbar.scss";
 import { Link } from "react-router-dom";
 
 export const NavbarHeader = () => {
   const [display, setDisplay] = useState(false);
-  const activeTab = window.location.hash;
-
   return (
-    <div className="container">
-      <nav className="navbar navbar-expand-lg">
-        <div className="container">
-          <button
-            onClick={() => setDisplay(!display)}
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarTogglerDemo02"
-            aria-controls="navbarTogglerDemo02"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+    <>
+      <div className="navbar-container">
+        <nav className="navbar">
+          <div className="navbar-section">
+            <ul className="navbar-nav">
               <li className="nav-item">
-                <a
-                  className={`nav-link ${
-                    activeTab === "#who" ? "active" : null
-                  } `}
-                  href="#who"
-                >
+                <a className={`nav-link`} href="#who">
                   Who I am?
                 </a>
               </li>
               <li className="nav-item">
-                <a
-                  className={`nav-link ${
-                    activeTab === "#projects" ? "active" : null
-                  } `}
-                  href="#projects"
-                >
+                <a className={`nav-link`} href="#projects">
                   Projects
                 </a>
               </li>
               {/* <li className="nav-item">
                 <a
                   className={`nav-link ${
-                    activeTab === "#experience" ? "active" : null
+                    activeTab === "#experience" ? "active-tab" : null
                   } `}
                   href="#experience"
                 >
@@ -56,42 +32,27 @@ export const NavbarHeader = () => {
                 </a>
               </li> */}
               <li className="nav-item">
-                <a
-                  className={`nav-link ${
-                    activeTab === "#contact" ? "active" : null
-                  } `}
-                  href="#contact"
-                >
+                <a className={`nav-link`} href="#contact">
                   Contact
                 </a>
               </li>
               <li className="nav-item">
-                <a
-                  className={`nav-link ${
-                    activeTab === "#background" ? "active" : null
-                  } `}
-                  href="#background"
-                >
+                <a className={`nav-link`} href="#background">
                   Certifications
                 </a>
               </li>
               <Link to="/resume">
-              <li className="nav-item">
-                <a
-                  className="nav-link"
-                  href="#background"
-                >
-                  Resume
-                </a>
-              </li>{" "}
+                <li className="nav-item">
+                  <a className="nav-link">Resume</a>
+                </li>{" "}
               </Link>
             </ul>
             <div className="d-flex" role="search">
-             <SocialIcons />
+              <SocialIcons />
             </div>
           </div>
-        </div>
-      </nav>
-    </div>
+        </nav>
+      </div>
+    </>
   );
 };
