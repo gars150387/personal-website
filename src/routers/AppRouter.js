f041bd6b0a13c899d312958f878adf666d9b3f1f
gars150387 <gar.santeliz@gmail.com>
2023-01-07 21:14:29 -0500

    routes updated

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,7 +1,5 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import { Home } from "../pages/Home";
 import { MainPage } from "../pages/main-page";
 import { Resume } from "../pages/Resume";
 
@@ -9,8 +7,8 @@ export const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/main" element={<MainPage />} />
+        {/* <Route exact path="/" element={<Home />} /> */}
+        <Route exact path="/" element={<MainPage />} />
         <Route exact path="/resume" element={<Resume />} />
       </Routes>
     </BrowserRouter>
